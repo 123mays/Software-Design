0b98fefb5dfa1d63abed87d6dfae16bb09387ad3
Mays Neiroukh <136735815+123mays@users.noreply.github.com>
2024-04-22 17:21:29 -0500

    Update createtables.sql

--- pythonlab/createtables.sql
+++ pythonlab/createtables.sql
@@ -1,1 +1,13 @@
+CREATE TABLE us_cities (
+    city VARCHAR(100),
+    state VARCHAR(100),
+    population INT,
+    latitude FLOAT,
+    longitude FLOAT
+);
 
+CREATE TABLE us_states (
+    state_code VARCHAR(2),
+    state_name VARCHAR(100),
+    population INT
+);
