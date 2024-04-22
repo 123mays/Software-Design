062a66a5cdfadee789839dd3c576b53c02c87b3b
Mays Neiroukh <136735815+123mays@users.noreply.github.com>
2024-04-22 17:20:26 -0500

    Update createtables.py

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,1 +1,55 @@
+CREATE TABLE us_cities (
+    city VARCHAR(100),
+    state VARCHAR(100),
+    population INT,
+    latitude FLOAT,
+    longitude FLOAT
+);
+
+CREATE TABLE us_states (
+    state_code VARCHAR(2),
+    state_name VARCHAR(100),
+    population INT
+);
+
+
+import psycopg2
+
+def create_tables():
+    commands = [
+        """
+        CREATE TABLE us_cities (
+            city VARCHAR(100),
+            state VARCHAR(100),
+            population INTEGER,
+            latitude FLOAT,
+            longitude FLOAT
+        );
+        """,
+        """
+        CREATE TABLE us_states (
+            state_code VARCHAR(2),
+            state_name VARCHAR(100),
+            population INTEGER
+        );
+        """
+    ]
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            dbname='yourdbname', user='yourusername', password='yourpassword', host='yourhost'
+        )
+        cur = conn.cursor()
+        for command in commands:
+            cur.execute(command)
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not limited:
+            conn.close()
+
+if __name__ == '__main__':
+    create_tables()
 
