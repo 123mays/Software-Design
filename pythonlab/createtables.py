a25467f468079ec9a8367cafd7be84699ceafac4
Mays Neiroukh <136735815+123mays@users.noreply.github.com>
2024-04-23 14:38:00 -0500

    Update createtables.py

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,6 +1,7 @@
 import psycopg2
-def create_tables():
-    commands = [
+
+def create_tables_and_load_data():
+    table_commands = [
         """
         CREATE TABLE us_cities (
             city VARCHAR(100),
@@ -18,22 +19,48 @@ def create_tables():
         );
         """
     ]
+    
+    copy_commands = [
+        """
+        COPY us_cities(city, state, population, latitude, longitude)
+        FROM '../us-cities-top-1k.csv' 
+        DELIMITER ',' 
+        CSV HEADER;
+        """,
+        """
+        COPY us_states(state_code, state_name, population)
+        FROM '../us-state-pop.csv' 
+        DELIMITER ',' 
+        CSV HEADER;
+        """
+    ]
+    
     conn = None
     try:
+        # Connect to the PostgreSQL database
         conn = psycopg2.connect(
             dbname='yourdbname', user='yourusername', password='yourpassword', host='yourhost'
         )
         cur = conn.cursor()
-        for command in commands:
+        
+        # Create tables
+        for command in table_commands:
             cur.execute(command)
-        cur.close()
+        
+        # Load data into tables from CSV files
+        for command in copy_commands:
+            cur.execute(command)
+        
+        # Commit the changes
         conn.commit()
+        
+        # Close communication with the database
+        cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
-        if conn is not limited:
+        if conn is not None:
             conn.close()
 
 if __name__ == '__main__':
-    create_tables()
-
+    create_tables_and_load_data()
