15e53da858c694a560eeaaa0251e81dd940db92c
Mays Neiroukh <136735815+123mays@users.noreply.github.com>
2024-04-23 19:35:34 -0500

    Update createtables.py

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,40 +1,17 @@
-import psycopg2
+DROP TABLE IF EXISTS us_cities;
+DROP TABLE IF EXISTS us_states;
+CREATE TABLE us_cities (
+    city VARCHAR(100),
+    state VARCHAR(100),
+    population INT,
+    latitude FLOAT,
+    longitude FLOAT
+);
 
-def create_tables_and_load_data():
-    table_commands = [
-        """
-        CREATE TABLE us_cities (
-            city VARCHAR(100),
-            state VARCHAR(100),
-            population INTEGER,
-            latitude FLOAT,
-            longitude FLOAT
-        );
-        """,
-        """
-        CREATE TABLE us_states (
-            state_code VARCHAR(2),
-            state_name VARCHAR(100),
-            population INTEGER
-        );
-        """
-    ]
-    
-    copy_commands = [
-        """
-        COPY us_cities(city, state, population, latitude, longitude)
-        FROM './us-cities-top-1k.csv' 
-        DELIMITER ',' 
-        CSV HEADER;
-        """,
-        """
-        COPY us_states(state_code, state_name, population)
-        FROM './us-state-pop.csv' 
-        DELIMITER ',' 
-        CSV HEADER;
-        """
-    ]
-    
-    
-if __name__ == '__main__':
-    create_tables_and_load_data()
+CREATE TABLE us_states (
+    state_code VARCHAR(2),
+    state_name VARCHAR(100),
+    population INT
+);
+
+
